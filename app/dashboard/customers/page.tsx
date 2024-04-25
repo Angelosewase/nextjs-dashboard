5a109d044178082add2b8d1b0d4cde2695742fd1
Angelosewase <sewasejo8@gmail.com>
2024-04-25 20:45:44 +0200

    update content of the main page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Metadata } from 'next';
  
 export const metadata: Metadata = {
-  title: 'fcustomers page',
+  title: 'customers page',
 };
 
 const Page = () => {
--- app/page.tsx
+++ app/page.tsx
@@ -18,11 +18,11 @@ export default function Page() {
             className={styles.shape}
           />
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className}`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
+            <strong>Welcome to Acme.</strong> This is the my nextjs dashboard project .
+            {/* <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
-            , brought to you by Vercel.
+            , brought to you by Vercel. */}
           </p>
           <Link
             href="/login"
